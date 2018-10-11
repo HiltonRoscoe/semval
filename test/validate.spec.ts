a06602a7f1ea75af5311dd57b901323235c1a651
John Dziurlaj <john@hiltonroscoe.com>
2018-10-11 12:52:05 -0400

    setup testing

---
+++ test/validate.spec.ts
@@ -0,0 +1,14 @@
+import { expect } from "chai";
+// -------------------------------------------------------------------------
+// Setup
+// -------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------
+// Specifications: common decorators
+// -------------------------------------------------------------------------
+// tslint:disable:only-arrow-functions
+describe("conditional validation", async function () {
+    it("should validate successfully on implication constraint", async function () {
+        expect(0).to.equal(0);
+    });
+});
