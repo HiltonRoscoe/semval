fc6dec6e59df2d903cf3eb9172475fc96f9d723c
John Dziurlaj <john@hiltonroscoe.com>
2018-10-29 10:06:56 -0400

    fix commander usage

--- src/index.ts
+++ src/index.ts
@@ -3,44 +3,47 @@ import program from "commander";
 (global as any).window = undefined; // Needed to avoid exception at import..
 import { IOclConstraint } from "./interfaces";
 import { SemanticValidation } from "./SemanticValidation";
-// simplifies uses test data
+// simplifies usage of test data
 // tslint:disable:no-var-requires
 
-// list of constraints, could be its own JSON file
-const constraints: IOclConstraint[] = require("../testData/oclrules2.json");
-const rules = constraints;
 // CLI UI stuff
 program
     .version("0.1.0")
     .description("ocl ruleset runner for json")
-    .option("-o, --oclRules <s>", "ocl rule set")
-    .option("-e, --enumerations <s>", "enumeration spec")
-    .option("-i, --instance <s>", "json instance")
+    .arguments("<instance> <oclRules> [enums]")
     .option("-m, --multiple", "instance file contains multiple instances")
-    .parse(process.argv);
-const instToTest = (() => {
-    const instances = require(program.instance || "../testData/oclInstances.json");
-    if (program.multiple) {
-        return instances;
+    .action((pInstance, pOclRules, pEnums, opts) => {
+        const instToTest = (() => {
+            const instances = require(pInstance);
+            if (program.multiple) {
+                return instances;
+            } else {
+                // downstream code expects name / value pairs
+                return { "(unnamed)": instances };
+            }
+        })();
+        // get in format to call semval
+        const oclRules: IOclConstraint[] = (pOclRules && require(pOclRules));
+        const enumerations = pEnums && require(pEnums);
+
+        const failures = SemanticValidation.validateInstance(instToTest, oclRules, enumerations);
+
+        // get all the invs that failed, only way we know they were used
+        const failedInvs =
+            failures.reduce((prev, curr) => {
+                return prev.concat(curr);
+            }).map((o) => o.invName);
+        // get all the constraints that were untested
+        const untestedCons = oclRules.filter((o) => failedInvs.indexOf(o.name) === -1).map((o) => o.name);
+
+        console.log("Coverage");
+        //console.log(JSON.stringify(failures));
+        console.log(color.green(`tested: ${JSON.stringify(failedInvs)}`));
+        console.log(color.red(`untested: ${JSON.stringify(untestedCons)}`));
+    });    
+    if (process.argv.length > 2) {
+        program.parse(process.argv);
     } else {
-        // downstream code expects name / value pairs
-        return { "(unnamed)": instances };
+        program.help();
     }
-})();
-// get in format to call semval
-const oclRules = (program.oclRules && require(program.oclRules)) || constraints;
-const enumerations = require(program.enumerations || "../testData/oclEnums.json");
-
-const failures = SemanticValidation.validateInstance(instToTest, oclRules, enumerations);
-
-// get all the invs that failed, only way we know they were used
-const failedInvs =
-    failures.reduce((prev, curr) => {
-        return prev.concat(curr);
-    }).map((o) => o.invName);
-// get all the constraints that were untested
-const untestedCons = constraints.filter((o) => failedInvs.indexOf(o.name) === -1).map((o) => o.name);
-console.log("Coverage");
-//console.log(JSON.stringify(failures));
-console.log(color.green(`tested: ${JSON.stringify(failedInvs)}`));
-console.log(color.red(`untested: ${JSON.stringify(untestedCons)}`));
+    
