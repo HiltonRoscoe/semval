6be7c0313bcbc39cbbd7c6ba971edcda530f6f8a
John Dziurlaj <john@hiltonroscoe.com>
2018-12-12 12:30:55 -0500

    make coverage report optional

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,11 @@ import prettyjson from "prettyjson";
 
 // CLI UI stuff
 program
-    .version("0.1.0")
+    .version("0.2.0")
     .description("ocl ruleset runner for json")
     .arguments("<instance> <oclRules> [enums]")
     .option("-m, --multiple", "instance file contains multiple instances")
+    .option("-c, --coverage", "append coverage report")
     .action((pInstance, pOclRules, pEnums, opts) => {
         const instToTest = (() => {
             const instances = require(pInstance);
@@ -27,24 +28,25 @@ program
         const enumerations = pEnums && require(pEnums);
 
         const failures = SemanticValidation.validateInstance(instToTest, oclRules, enumerations);
-
-        const failedInvs = (() => {
-            if (failures.some((o) => o.length > 0)) {
-                // get all the invs that failed, only way we know they were used
-                return failures.reduce((prev, curr) => {
-                    return prev.concat(curr);
-                }, []).map((o) => o.invName);
-                // get all the constraints that were untested
-            } else {
-                return [];
-            }
-        })()
-        const untestedCons = oclRules.filter((o) => failedInvs.indexOf(o.name) === -1).map((o) => o.name);
-        console.log("Coverage");
-        // if an OCL invariant failed, we know it was covered by an instance
-        console.log(prettyjson.render(failedInvs, { emptyArrayMsg: "No OCL constraints covered" }));
-        // the ones that didn't work
-        console.log(prettyjson.render(untestedCons, { emptyArrayMsg: "All OCL constraints covered", dashColor: "red" }));
+        if (program.coverage) {
+            const failedInvs = (() => {
+                if (failures.some((o) => o.length > 0)) {
+                    // get all the invs that failed, only way we know they were used
+                    return failures.reduce((prev, curr) => {
+                        return prev.concat(curr);
+                    }, []).map((o) => o.invName);
+                    // get all the constraints that were untested
+                } else {
+                    return [];
+                }
+            })()
+            const untestedCons = oclRules.filter((o) => failedInvs.indexOf(o.name) === -1).map((o) => o.name);
+            console.log("Coverage");
+            // if an OCL invariant failed, we know it was covered by an instance
+            console.log(prettyjson.render(failedInvs, { emptyArrayMsg: "No OCL constraints covered" }));
+            // the ones that didn't work
+            console.log(prettyjson.render(untestedCons, { emptyArrayMsg: "All OCL constraints covered", dashColor: "red" }));
+        }
     });
 if (process.argv.length > 2) {
     program.parse(process.argv);
